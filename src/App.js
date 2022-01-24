961fc213e008d390b36134cc562f8047df754849
casder <casderiopus@gmail.com>
2022-01-25 00:46:02 +0300

    style: edit code outlook

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 
 const App = () => {
-  return (
-    <div className="App">
+    return (
+        <div className="App">
 
-    </div>
-  );
+        </div>
+    );
 }
 
 export default App;
