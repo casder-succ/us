3cb34a2ce1320f3f662c00080756fe180128801a
casder <casderiopus@gmail.com>
2022-01-25 01:16:59 +0300

    feat: integrate components

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,9 @@ import React from 'react';
 const Header = () => {
     return (
         <div>
-            
+            <a href="#s">Home</a>
+            <a href="#s">News Feed</a>
+            <a href="#s">Messages</a>
         </div>
     );
 };
