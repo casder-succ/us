46997a32b862b4e348a76a2f04254546e4291e53
casder <casderiopus@gmail.com>
2022-02-25 14:40:49 +0300

    refactor: rebase components to separate directory

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div>
+                Home
+            </div>
+            <div>
+                News Feed
+            </div>
+            <div>
+                Messages
+            </div>
+        </div>
+    );
+};
+
+export default Header;
