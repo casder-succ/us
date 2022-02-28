633a0271f897e09d3190e7c19b293755623de595
casder <casderiopus@gmail.com>
2022-02-28 23:58:46 +0300

    refactor: integrate new component

---
+++ src/components/Profile/OwnPosts/NewPost/NewPost.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import s from './NewPost.module.scss';
+
+const NewPost = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default NewPost;
--- src/components/Profile/OwnPosts/OwnPosts.jsx
+++ src/components/Profile/OwnPosts/OwnPosts.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import s from './OwnPosts.module.scss';
 import Post from "./Post/Post";
+import NewPost from "./NewPost/NewPost";
 
 const OwnPosts = () => {
     return (
-        <div>
-            new post
+        <div className={s.posts}>
+            <NewPost/>
             <Post message = 'Hello' likes='123'/>
             <Post message = 'Hi guys no to war' likes='20'/>
         </div>
