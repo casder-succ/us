60532a9a6d1f17559cd7144a5098df90a8843f96
casder <casderiopus@gmail.com>
2022-02-28 22:27:19 +0300

    refactor: create base structure for components

---
+++ src/components/Messages/DialogItem.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import s from './Messages.module.scss';
+
+const DialogItem = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default DialogItem;
