e1df1b6967779be701cc6ae430fb881e401075f5
casder <casderiopus@gmail.com>
2022-03-01 00:07:45 +0300

    refactor: separate profile info

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import s from './Profile.module.scss';
 import OwnPosts from "./OwnPosts/OwnPosts";
+import ProfileInfo from "./ProfileInfo/ProfileInfo";
 
 const Profile = () => {
     return (
-        <div className={s.content}>
-            <div>
-                ava + descr
-            </div>
+        <div className={s.profile}>
+            <ProfileInfo/>
             <OwnPosts/>
         </div>
     );
