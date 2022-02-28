54957adee9ff452349a80c83e57cdced8121ea5a
casder <casderiopus@gmail.com>
2022-02-28 22:37:35 +0300

    refactor: add new components

---
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import s from './ProfilInfo.module.scss';
+
+const ProfileInfo = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default ProfileInfo;
