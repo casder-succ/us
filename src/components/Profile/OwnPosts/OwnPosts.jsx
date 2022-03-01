a9e2396edec4d540619c2f5b307c89c985c89597
casder <casderiopus@gmail.com>
2022-03-01 23:44:53 +0300

    feat: separate datas and add templating

--- src/components/Profile/OwnPosts/OwnPosts.jsx
+++ src/components/Profile/OwnPosts/OwnPosts.jsx
@@ -4,11 +4,19 @@ import Post from "./Post/Post";
 import NewPost from "./NewPost/NewPost";
 
 const OwnPosts = () => {
+    const posts = [
+        {message: "hello", likes: 123},
+        {message: "Hi guys no to war", likes: 20},
+        {message: "was sup niga", likes: 23},
+    ]
+
     return (
         <div className={s.posts}>
             <NewPost/>
-            <Post message = 'Hello' likes='123'/>
-            <Post message = 'Hi guys no to war' likes='20'/>
+            {posts
+                .map((el, i) =>
+                    <Post message={el.message} likes={el.likes} key={i}/>
+                )}
         </div>
 
     );
