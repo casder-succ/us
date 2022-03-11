9de4112e1ddb5852d6f54d8655673e83e5777cd7
casder <casderiopus@gmail.com>
2022-03-11 17:42:16 +0300

    refactor: move state to separate directory

--- src/index.js
+++ src/index.js
@@ -4,31 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom";
-
-const posts = [
-    {message: "hello", likes: 123},
-    {message: "Hi guys no to war", likes: 20},
-    {message: "was sup niga", likes: 23},
-];
-const dialogItems = [
-    {name: "Kirill", id: "1"},
-    {name: "Dima", id: "2"},
-    {name: "Ksusha", id: "3"},
-    {name: "Misha", id: "4"},
-    {name: "Artyom", id: "5"},
-    {name: "Vlad", id: "6"},
-];
-
-const messages = [
-    {message: "hi"},
-    {message: "how are you"},
-    {message: "hello"},
-];
+import * as state from './store/state';
 
 ReactDOM.render(
     <React.StrictMode>
         <BrowserRouter>
-            <App posts={posts} messages={messages} dialogs={dialogItems}/>
+            <App posts={state.posts} messages={state.messages} dialogs={state.dialogItems}/>
         </BrowserRouter>
     </React.StrictMode>,
     document.getElementById('root')
