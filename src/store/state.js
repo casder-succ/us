20ff70ce1b1b941c82b1cc0a2f678e0a1bf95914
casder <casderiopus@gmail.com>
2022-03-11 18:08:25 +0300

    refactor: unite state branches

--- src/store/state.js
+++ src/store/state.js
@@ -1,19 +1,26 @@
-export const posts = [
-    {message: "hello", likes: 123},
-    {message: "Hi guys no to war", likes: 20},
-    {message: "was sup niga", likes: 23},
-];
-export const dialogItems = [
-    {name: "Kirill", id: "1"},
-    {name: "Dima", id: "2"},
-    {name: "Ksusha", id: "3"},
-    {name: "Misha", id: "4"},
-    {name: "Artyom", id: "5"},
-    {name: "Vlad", id: "6"},
-];
 
-export const messages = [
-    {message: "hi"},
-    {message: "how are you"},
-    {message: "hello"},
-];
+
+export const state = {
+    ProfilePage: {
+        posts: [
+            {message: "hello", likes: 123},
+            {message: "Hi guys no to war", likes: 20},
+            {message: "was sup niga", likes: 23},
+        ]
+    },
+    DialogsPage: {
+        messages: [
+            {message: "hi"},
+            {message: "how are you"},
+            {message: "hello"},
+        ],
+        dialogs: [
+            {name: "Kirill", id: "1"},
+            {name: "Dima", id: "2"},
+            {name: "Ksusha", id: "3"},
+            {name: "Misha", id: "4"},
+            {name: "Artyom", id: "5"},
+            {name: "Vlad", id: "6"},
+        ],
+    }
+}
