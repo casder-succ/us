6df6458af875a668f3548d0c63faf616c50aed12
casder <casderiopus@gmail.com>
2022-04-29 10:27:48 +0300

    feat: add form and functionality for sending message

--- src/components/Messages/Dialogs.jsx
+++ src/components/Messages/Dialogs.jsx
@@ -2,8 +2,18 @@ import React from 'react';
 import s from './Dialogs.module.scss'
 import DialogItem from "./DialodItem/DialogItem";
 import Message from "./Message/Message";
+import {sendMessageActionCreator, updateNewMessageTextActionCreator} from "../../store/state";
 
 const Dialogs = (props) => {
+    const onSend = () => {
+        props.dispatch(sendMessageActionCreator())
+    }
+
+    const onMessageTextUpdate = (e) => {
+        const message = e.target.value;
+        props.dispatch(updateNewMessageTextActionCreator(message));
+    }
+
     return (
         <div className={s.dialogs}>
             <div>
@@ -13,10 +23,17 @@ const Dialogs = (props) => {
                     )}
             </div>
             <div>
-                {props.state.messages
-                    .map((el, i) =>
-                        <Message message={el.message} key={i}/>
-                    )}
+                <div>{props.state.messages.map((el, i) => <Message message={el.message} key={i}/>)}</div>
+                <div>
+                    <textarea
+                        onChange={onMessageTextUpdate}
+                        value={props.state.newMessageText}
+                        placeholder="Enter your message.."
+                        id="" cols="30" rows="10"/>
+                </div>
+                <div>
+                    <button onClick={onSend}>Send</button>
+                </div>
             </div>
         </div>
     );
