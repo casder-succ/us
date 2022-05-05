6a3975a4acfb40b8df3e6308793398a90f2d7c67
casder <casderiopus@gmail.com>
2022-05-05 22:11:48 +0300

    refactor: plug redux provider

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import './index.css';
 import App from './App';
 import {BrowserRouter} from "react-router-dom";
 import {store} from "./store/reduxStore";
+import {Provider} from "react-redux";
 
 const renderContent = (state) => {
     ReactDOM.render(
         <React.StrictMode>
             <BrowserRouter>
-                <App store={store}/>
+                <Provider store={store}>
+                    <App store={store}/>
+                </Provider>
             </BrowserRouter>
         </React.StrictMode>,
         document.getElementById('root')
